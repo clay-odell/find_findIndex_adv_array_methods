ddd584b333333516b5fd5d04f0fa6f230ded0761
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-01 15:43:30 -0600

    added script to html, findUserByUserName exercise completed

--- find.js
+++ find.js
@@ -8,9 +8,25 @@ function myFind (arr, callback){
 
 function myFindIndex (arr, callback){
     for (let i=0; i<arr.length; i++) {
-        if (callback(arr[i], i, arr)) === true {
+        if (callback(arr[i], i, arr) === true) {
             return i;
         }
     }
     return -1;
 }
+
+function findUserByUsername(arr, username) {
+    return arr.find(function(users) {
+        return users.username === username;
+    })
+}
+const users = [
+    {username: 'mlewis'},
+    {username: 'akagen'},
+    {username: 'msmith'}
+  ];
+  
+  const findAnswer1 = findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+  const findAnswer2 = findUserByUsername(users, 'taco') // undefined
+
+  
